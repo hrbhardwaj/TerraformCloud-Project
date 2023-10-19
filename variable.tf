2807d7226a156d2ec382172ae9249f181437c127
hrbhardwaj <131919525+hrbhardwaj@users.noreply.github.com>
2023-10-19 16:52:11 -0400

    Update variable.tf
    ok

--- variable.tf
+++ variable.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key" {
+  type    = string
+  default = ""
+}
+
+variable "aws_secret_key" {
+  type    = string
+  default = ""
+}
+
 variable "region" {
    default   = "us-east-1"
 }
