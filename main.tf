2018164916538934d7bf055c4c77737cc60f9c81
hrbhardwaj <131919525+hrbhardwaj@users.noreply.github.com>
2023-10-19 16:28:59 -0400

    Update main.tf
    ok

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 # main.tf
 
 provider "aws" {
-  region     = var.region
+  region = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "example" {
