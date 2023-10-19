9bbab3683843b0fd9be899b419db025cb2d3cb79
hrbhardwaj <131919525+hrbhardwaj@users.noreply.github.com>
2023-10-19 14:52:46 -0400

    Update main.tf
    ok

--- main.tf
+++ main.tf
@@ -3,33 +3,11 @@
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+  region     = var.region
 }
 
-provider "aws" {
-  region = var.region
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
+resource "aws_instance" "example" {
+   instance_type = var.instance_type
+   key_name      = var.key_name
+   ami           = var.ami
 }
