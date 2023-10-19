c163f295387cafd196761afa83e05e9485edc3e1
hrbhardwaj <131919525+hrbhardwaj@users.noreply.github.com>
2023-10-19 14:54:31 -0400

    Create variable.tf
    ok

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "region" {
+   default   = "us-east-1"
+}
+variable "instance_type" {
+   default   = "t2.micro"
+}
+
+variable "key_name" {
+   default   = "project1"
+}
+variable "ami" {
+   default   = "ami-0fc5d935ebf8bc3bc"
+}
